d3ebfe93bda419d1beaa050f4735afbe7daa1405
Hanyx115 <ahmedhanyx115@gmail.com>
2023-10-14 02:10:02 +0300

    final update + correct error program

---
+++ c#1/sheets assignaments/sheet 1/correct_errors/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace correct_errors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a)//
+            string age= Console.ReadLine();
+            if (int.Parse(age) >=65) {
+                Console.WriteLine("Age greater than or equal to 65");
+                    }
+            else {
+                Console.WriteLine("Age is less than 65");
+            }
+            //b)//
+            int x = 1;
+            int total=0;
+            while (total <= 10)
+            {
+                total += x;
+                ++x;
+            }
+            //c)//
+            while (total <= 10)
+            {
+                total += x;
+                ++x;
+            }
+            //d)//
+            int y=0;
+            while (y < 10){
+                Console.WriteLine(y);
+                ++y;
+            }
+        }
+    }
+}
