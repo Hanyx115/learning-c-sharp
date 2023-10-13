26b179de772e1c2f7dd51b4236a661f754c83576
Hanyx115 <ahmedhanyx115@gmail.com>
2023-10-14 01:48:55 +0300

    update

--- c#1/sheets assignaments/sheet 1/mystery/Program.cs
+++ c#1/sheets assignaments/sheet 1/mystery/Program.cs
@@ -10,6 +10,24 @@ namespace mystery
     {
         static void Main(string[] args)
         {
+
+            /*answer is 
+1        0
+1        2
+4        2
+4        6
+9        6
+9        12
+16       12
+16       20
+25       20
+25       30
+36       30
+36       42
+49       42
+49       56
+64       56
+*/
             int number = 1;
             int sum1 = 0 ; int sum2 = 0;
             while (number <= 15)
