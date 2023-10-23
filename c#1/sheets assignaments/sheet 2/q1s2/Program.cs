3bb2a59e733046bec3ec847db8099a613b050b5c
Hanyx115 <ahmedhanyx115@gmail.com>
2023-10-23 20:33:01 +0300

    update

--- c#1/sheets assignaments/sheet 2/q1s2/Program.cs
+++ c#1/sheets assignaments/sheet 2/q1s2/Program.cs
@@ -10,6 +10,43 @@ namespace q1s2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter how many times you want to change the temprature");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                Console.WriteLine("please insert '0' for converting fahrenhite to celcius or '1' for converting celcius to fahrenhite");
+                int ans = int.Parse(Console.ReadLine());
+                if (ans == 0)
+                {
+                    string Fahrenheit = Console.ReadLine();
+                    double fah = double.Parse(Fahrenheit);
+                    Console.WriteLine("celcius = " + cels(fah));
+                }
+                else if (ans == 1)
+                {
+                    string celcius = Console.ReadLine();
+                    double fah = double.Parse(celcius);
+                    Console.WriteLine("Fahrenhite = " + fehr(fah));
+
+                }
+                n = n - 1;
+
+
+
+            }
+        }
+        static double cels(double x)
+        {
+            double celsius = (5.0 / 9.0) * (x - 32);
+            return celsius;
+        }
+        static double fehr(double x)
+        {
+            double fah = (9.0 / 5.0) * (x + 32);
+            return fah;
+
         }
     }
 }
+  
