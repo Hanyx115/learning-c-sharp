8c8fc894c12dc5908a7b90d4463e03f9636146d5
Hanyx115 <ahmedhanyx115@gmail.com>
2023-10-25 23:46:47 +0300

    q3/4 s2

---
+++ c#1/sheets assignaments/sheet 2/q3s2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    public static int Lcm(int a, int b)
+    {
+        
+        return (a * b) / Gcd(a, b);
+    }
+
+    public static int Gcd(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void Main(string[] args)
+    {
+        
+        Console.Write("Enter the first integer: ");
+        if (int.TryParse(Console.ReadLine(), out int num1))
+        {
+            Console.Write("Enter the second integer: ");
+            if (int.TryParse(Console.ReadLine(), out int num2))
+            {
+                
+                int lcm = Lcm(num1, num2);
+                Console.WriteLine($"The Lowest Common Multiple of {num1} and {num2} is: {lcm}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for the second integer.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input for the first integer.");
+        }
+    }
+}
+
---
+++ c#1/sheets assignaments/sheet 2/q4s2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class Program
+{
+    private static bool IsPrime(int number)
+    {
+        if (number <= 1)
+            return false;
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Prime numbers less than 10,000:");
+        for (int i = 2; i < 10000; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
