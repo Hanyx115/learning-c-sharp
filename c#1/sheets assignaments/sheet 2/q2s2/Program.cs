880c4a13736a522cb0cb13dab889a82ca5b57d8f
Hanyx115 <ahmedhanyx115@gmail.com>
2023-10-25 23:06:59 +0300

    q2s2 fix errors

---
+++ c#1/sheets assignaments/sheet 2/q2s2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2s2
+{
+    internal class Program
+    {
+
+        //A)no access modifier used
+        public void G()
+        {
+            Console.WriteLine("inside g");
+        
+        }
+
+        private static void H()
+        {
+            Console.WriteLine("inside H");
+
+        }
+        //B)no returned value
+        public int sum(int x,int y)
+        {
+            return x + y;
+        }
+        //C)declaring variables with common name
+        public void F(float a)
+        {
+            float z;
+            Console.WriteLine(a);
+        
+        }
+
+        //D)returning value while diclaring method as void 
+        public int Product()
+        {
+            int a=6,b=5,c=4,result;
+            result = a * b * c;
+            Console.WriteLine("result is "+result);
+            return result;
+        }
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
