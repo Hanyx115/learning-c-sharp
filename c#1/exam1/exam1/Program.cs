5381392c142f6eb8425c2057c429e080c2799b44
Hanyx115 <ahmedhanyx115@gmail.com>
2023-11-19 14:02:50 +0200

    ghjjh

---
+++ c#1/exam1/exam1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 0;
+            int sum = 0;
+            int avg = 0;
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = 0;
+                sum = arr[i] + sum;
+                avg = sum / arr.Length;  
+
+
+            }
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > avg)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                if (arr[i] == arr[2])
+                {
+                    count++;
+                    
+
+                }
+
+
+            }
+            Console.WriteLine(count);
+        }
+
+
+
+        }
+
+}
---
+++ c#1/exam1/q2/CelestialBody.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    internal class CelestialBody
+    {
+        private string name;
+        private string type;
+
+        private string Children;
+        private string[] children = new string[5];
+
+        public CelestialBody(string name, string type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+        public string names { get; }
+
+        public string types { get; }
+
+        private string children1;
+
+        public string GetChildren()
+        {
+            return children1;
+        }
+
+        public void SetChildren(string value)
+        {
+            children1 = value;
+        }
+
+        public static void AddChild(string children)
+        { string child1 = Console.ReadLine();
+
+
+            if (children.Length == 5)
+            {
+                Console.WriteLine("full");
+
+            }
+            else
+            {
+                children += child1;
+            }
+
+        }
+        public string  Getchildren(string Children1)
+        {
+            int i = 0;
+            for (i = 0; i < children.Length; i++)
+            {
+                if (children[i] ==null )
+                {
+                    children[i] = Children1;
+                }
+                
+            }
+
+            return Children;
+        }
+
+
+
+
+
+
+
+    }
+}
