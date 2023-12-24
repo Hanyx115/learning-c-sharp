3fa1f047edf0808a9e2a2ee00fe23b4cf8232346
Hanyx115 <ahmedhanyx115@gmail.com>
2023-12-24 13:28:54 +0200

    twelfith exam

---
+++ Twelfth_exam/Twelfth_exam/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twelfth_exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Twelfth_exam/Twelfth_exam/person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twelfth_exam
+{
+    internal class person
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+        }
+        public person()
+        { 
+        
+        }
+        public person(string name)
+        {
+            this.name = name;
+        }
+        public virtual string Greet() 
+        {
+            return string.Empty;
+        
+        }
+        public virtual string SayBye() 
+        {
+            return "Goodbye";
+        }
+        
+    }
+    internal class Teacher : person
+    {
+        private int NumberOfCourses;
+        private double salary;
+        public Teacher(int numberofcourses, double salary, string Name) : base(Name)
+        {
+            this.NumberOfCourses = numberofcourses;
+            this.salary = salary;
+        }
+        public override string Greet()
+        {
+            return "Hello Students!";
+        }
+        public void teach(int numberofcourses)
+        {
+            Console.WriteLine("I Have To Teach"+ numberofcourses+"this semester");
+        }
+
+    }
+    internal class Student : person
+    {
+        private double GPA;
+        public Student(double GPA,string name):base (name)
+        {
+            this. GPA = GPA;
+
+        }
+        public override string Greet()
+        {
+            return "Hello Teacher";
+        }
+        public void learn() 
+        {
+            Console.WriteLine("I still have courses to finish!"); 
+        }
+    }
+}
